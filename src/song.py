35c7a15ef06283e226da07e6fe2df0dbacdaf133
Amna Bashir <amna.bashir2@gmail.com>
2021-11-26 12:20:47 +0000

    song class done

---
+++ src/song.py
@@ -0,0 +1,6 @@
+class Song:
+    def __init__(self,name,song_type):
+        self.name = name
+        self.type = song_type
+
+    
---
+++ tests/song_test.py
@@ -0,0 +1,14 @@
+import unittest
+from src.song import Song
+class TestSong(unittest.TestCase):
+    def setUp(self):
+        self.song = Song("Best Day","happy song")
+        
+
+    def test_song_has_nmae(self):
+        self.assertEqual("Best Day", self.song.name)
+
+    def test_song_has_type(self):
+        self.assertEqual("happy song",self.song.type)
+
+    
